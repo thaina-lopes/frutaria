3ca214bab9c5b2130f5dcafb416d4f1e794feb4d
Thaina <thainalopes@gmail.com>
2024-06-10 09:50:40 -0300

    Adição dos ícones

--- src/paginas/app.js
+++ src/paginas/app.js
@@ -1,15 +1,21 @@
+import Botoes from "../componentes/botoes";
 import logo from "../img/fruta04.png";
 import styles from "../paginas/app.module.css";
 
 export default function App() {
   return (
     <div className={styles.app}>
-      <div className={styles.imagemLogo}>
-        <img src={logo} className={styles.logo} alt="logo" />
+      <div className={styles.conteudo}>
+        <div className={styles.imagemLogo}>
+          <img src={logo} className={styles.logo} alt="logo" />
+        </div>
+        <div className={styles.textoLogo}>
+          <h1 className={styles.titulo}>Frutaria</h1>
+          <p className={styles.paragrafo}>Hortifrúti on-line</p>
+        </div>
       </div>
-      <div className={styles.textoLogo}>
-        <h1 className={styles.titulo}>Frutaria</h1>
-        <p className={styles.paragrafo}>Hortifrúti on-line</p>
+      <div className={styles.botao}>
+        <Botoes>Entrar</Botoes>
       </div>
     </div>
   );
