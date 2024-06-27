9176af8fca292b1b4fcb3ecd307321f8eef810d8
Thaina <thainalopes@gmail.com>
2024-06-27 12:24:20 -0300

    Implemente direcionamento após login bem sucedido

--- src/paginas/Login/login.js
+++ src/paginas/Login/login.js
@@ -2,27 +2,56 @@ import styles from "../Login/login.module.css";
 import logo from "../../img/fruta03.png";
 import Botao from "../../componentes/botao";
 import { Link } from "react-router-dom";
-import BotaoPequeno from "../../componentes/botaoPequeno";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 export default function Login() {
+  const [usuario, setUsuario] = useState("");
+  const [senha, setSenha] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!usuario || !senha) {
+      alert("Os campos de usuário e senha precisam der preenchidos");
+      return;
+    }
+    if (usuario === "Maria" && senha === "123") {
+      navigate("/home");
+    } else {
+      alert("Usuário ou senha incorretos!");
+    }
+  };
+
   return (
     <div className={styles.login}>
       <img src={logo} alt="logo com frutas" className={styles.logo} />
       <div className={styles.loginContainer}>
         <h2 className={styles.titulo}>Login</h2>
         <p className={styles.subtitulo}>Insira seu e-mail e sua senha</p>
-        <form className={styles.formulario}>
+        <form onSubmit={handleSubmit} className={styles.formulario}>
           <label className={styles.input}>
-            E-mail
-            <input type="text" placeholder="Digite seu e-mail" />
+            Nome de usuário
+            <input
+              type="text"
+              value={usuario}
+              onChange={(e) => setUsuario(e.target.value)}
+              placeholder="Digite seu nome"
+            />
           </label>
           <label className={styles.input}>
             Senha
-            <input type="password" placeholder="Digite sua senha" />
+            <input
+              type="password"
+              value={senha}
+              onChange={(e) => setSenha(e.target.value)}
+              placeholder="Digite sua senha"
+            />
           </label>
+          <Botao type="submit">Ok</Botao>
         </form>
       </div>
-      <Botao>Ok</Botao>
       <p className={styles.novaConta}>
         Não tem uma conta?{" "}
         <Link to="/cadastro" className={styles.cadastrarNovaConta}>
